ecded52a409b53a628083df09bdb2a28b16efbf8
Shobana <shobanam@thoughtworks.com>
2013-08-04 17:58:10 +0530

    Broadcastresceiver working for incoming message with an empty start up activity.

---
+++ src/com/example/personalAssistant/StartUp.java
@@ -0,0 +1,11 @@
+package com.example.personalAssistant;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartUp extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
